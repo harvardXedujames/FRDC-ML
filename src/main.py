c77943b6dac9dc3827cd3a101489c27b79de304b
Evening <johnchangqi@hotmail.com>
2023-09-13 11:11:06 +0800

    Move test to pytest

--- src/main.py
+++ src/main.py
@@ -1,21 +1,4 @@
 """ Entry point for the whole ML pipeline. """
-from pathlib import Path
 
-import numpy as np
-from PIL import Image
-
-from evaluate.evaluate import dummy_evaluate
-from load import load_image
-from preprocess.preprocess import dummy_preprocess
-from train.train import dummy_train
-
-ar = np.random.randint(0, 255, (100, 50, 3), dtype=np.uint8)
-IMAGE_NAME = Path("test.png")
-Image.fromarray(ar).save(IMAGE_NAME)
-
-ar_im = load_image(IMAGE_NAME)
-ar_preproc = dummy_preprocess(ar_im)
-model = dummy_train(ar_preproc)
-evaluate = dummy_evaluate(model, ar_preproc)
-
-IMAGE_NAME.unlink(missing_ok=True)
+if __name__ == '__main__':
+    print(729)
