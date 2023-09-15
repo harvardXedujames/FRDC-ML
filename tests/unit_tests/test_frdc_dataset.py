186cdca7635c19128d9676daf5bbcc70f4305301
Evening <johnchangqi@hotmail.com>
2023-09-15 12:45:17 +0800

    Separate debug dataset download test

--- tests/unit_tests/test_frdc_dataset.py
+++ tests/unit_tests/test_frdc_dataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from frdc.conf import DATASET_FILE_NAMES
@@ -13,11 +14,18 @@ def test_download_datasets(ds):
     ds.download_datasets(dryrun=True)
 
 
+def test_download_dataset(ds):
+    ds.download_dataset(site='DEBUG', date='0', version=None)
+
+
 def test_list_datasets(ds):
     df = ds.list_gcs_datasets()
     assert len(df) > 0
 
 
-def test_download_dataset(ds):
+def test_load_dataset(ds):
+    # Loading the debug dataset indirectly tests the load_dataset method.
     dataset = ds._load_debug_dataset()
     assert len(dataset) == len(DATASET_FILE_NAMES)
+    for value in dataset.values():
+        assert isinstance(value, np.ndarray)
