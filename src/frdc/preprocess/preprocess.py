8f3ee578f21f7fb619149974af64936f759aadf7
Evening <johnchangqi@hotmail.com>
2023-09-15 15:20:08 +0800

    Remove Dummy preproc

--- src/frdc/preprocess/preprocess.py
+++ src/frdc/preprocess/preprocess.py
@@ -9,19 +9,6 @@ from sklearn.preprocessing import minmax_scale
 from frdc.conf import Band
 
 
-def dummy_preprocess(ar: np.ndarray):
-    """ A dummy preprocessing function. This simply takes an ar and copies it on another axis
-
-    Args:
-        ar: Image NDArray, must be of shape (H, W, C)
-
-    Returns:
-        A list of preprocessed NDArray, of shape (H, W, C) each.
-
-    """
-    return [ar, ar]
-
-
 def compute_crown_masks(
         bands_dict: dict[str, np.ndarray],
         nir_threshold_value=0.5,
