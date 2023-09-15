f9c0cd9f9491eb6c1003d0d41ba2d42ef4b64962
Evening <johnchangqi@hotmail.com>
2023-09-15 12:49:53 +0800

    Update dummy integration test

--- tests/integration_tests/test_dummy.py
+++ tests/integration_tests/test_dummy.py
@@ -1,22 +1,12 @@
-from pathlib import Path
-
-import numpy as np
-from PIL import Image
-
+from frdc.conf import DATASET_FILE_NAMES
 from frdc.evaluate.evaluate import dummy_evaluate
-from frdc.load import load_image
+from frdc.load import FRDCDataset
 from frdc.preprocess import dummy_preprocess
 from frdc.train.train import dummy_train
 
 
 def test_pipeline():
-    ar = np.random.randint(0, 255, (100, 50, 3), dtype=np.uint8)
-    IMAGE_NAME = Path("test.png")
-    Image.fromarray(ar).save(IMAGE_NAME)
-
-    ar_im = load_image(IMAGE_NAME)
-    ar_preproc = dummy_preprocess(ar_im)
+    ar_im = FRDCDataset()._load_debug_dataset()
+    ar_preproc = dummy_preprocess(ar_im[DATASET_FILE_NAMES[0]])
     model = dummy_train(ar_preproc)
     evaluate = dummy_evaluate(model, ar_preproc)
-
-    IMAGE_NAME.unlink(missing_ok=True)
