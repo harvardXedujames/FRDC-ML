80d6939b186b455db67e639b144e0e4aac1e1a75
Evening <johnchangqi@hotmail.com>
2023-09-20 13:22:32 +0800

    Improve segment extraction method tests

--- tests/unit_tests/preprocess/test_extract_segments.py
+++ tests/unit_tests/preprocess/test_extract_segments.py
@@ -1,19 +1,38 @@
+""" Test Segment Extraction methods.
+
+We check if the cropping works:
+If we crop, then >= 1 shapes in the output must be different from the input.
+Else,       then ALL  shapes in the output must be the same as the input.
+
+For example, given that our input image 10 x 10:
+- If we crop, then we expect at least one of the output images to be < 10 x 10, for example 7 x 5
+- If we don't crop, then we expect all of the output images to be 10 x 10
+
+Thus, our test is:
+- If we crop,       any(segment.shape != ar.shape for segment in segments)
+- If we don't crop, all(segment.shape == ar.shape for segment in segments)
+"""
+
 from frdc.preprocess import extract_segments_from_bounds, extract_segments_from_labels, compute_labels
 
 
 def test_extract_segments_from_bounds_cropped(ds):
-    extract_segments_from_bounds(ds.get_ar_bands(), ds.get_bounds(), cropped=True)
+    segments = extract_segments_from_bounds(ar := ds.get_ar_bands(), ds.get_bounds(), cropped=True)
+    assert any(segment.shape != ar.shape for segment in segments)
 
 
 def test_extract_segments_from_bounds_no_crop(ds):
-    extract_segments_from_bounds(ds.get_ar_bands(), ds.get_bounds(), cropped=False)
+    segments = extract_segments_from_bounds(ar := ds.get_ar_bands(), ds.get_bounds(), cropped=False)
+    assert all(segment.shape == ar.shape for segment in segments)
 
 
 def test_extract_segments_from_labels_cropped(ds):
     ar_labels = compute_labels(ds.get_ar_bands())
-    extract_segments_from_labels(ds.get_ar_bands(), ar_labels, cropped=True)
+    segments = extract_segments_from_labels(ar := ds.get_ar_bands(), ar_labels, cropped=True)
+    assert any(segment.shape != ar.shape for segment in segments)
 
 
 def test_extract_segments_from_labels_no_crop(ds):
     ar_labels = compute_labels(ds.get_ar_bands())
-    extract_segments_from_labels(ds.get_ar_bands(), ar_labels, cropped=False)
+    segments = extract_segments_from_labels(ar := ds.get_ar_bands(), ar_labels, cropped=False)
+    assert all(segment.shape == ar.shape for segment in segments)
