b5429865cbc2f114d77149785a7c5c59c59fafe8
Evening <johnchangqi@hotmail.com>
2023-10-10 20:59:20 +0800

    Update conf.py

--- src/frdc/conf.py
+++ src/frdc/conf.py
@@ -7,25 +7,24 @@ SECRETS_DIR = ROOT_DIR / '.secrets'
 GCS_PROJECT_ID = 'frmodel'
 GCS_BUCKET_NAME = 'frdc-scan'
 
-# These are sorted by wavelength
 BAND_CONFIG = OrderedDict({
-    'WR': ('*result.tif', lambda x: x[..., 0:1]),
-    'WG': ('*result.tif', lambda x: x[..., 1:2]),
     'WB': ('*result.tif', lambda x: x[..., 2:3]),
-    'NR': ('result_Red.tif', lambda x: x),
-    'NG': ('result_Green.tif', lambda x: x),
+    'WG': ('*result.tif', lambda x: x[..., 1:2]),
+    'WR': ('*result.tif', lambda x: x[..., 0:1]),
     'NB': ('result_Blue.tif', lambda x: x),
+    'NG': ('result_Green.tif', lambda x: x),
+    'NR': ('result_Red.tif', lambda x: x),
     'RE': ('result_RedEdge.tif', lambda x: x),
     'NIR': ('result_NIR.tif', lambda x: x),
 })
 
-# DEFAULT_BAND_SCALED_CONFIG = OrderedDict({
-#     'WR': ('*result.tif', lambda x: x[..., 0:1] / 2 ** 8),
-#     'WG': ('*result.tif', lambda x: x[..., 1:2] / 2 ** 8),
-#     'WB': ('*result.tif', lambda x: x[..., 2:3] / 2 ** 8),
-#     'NR': ('result_Red.tif', lambda x: x / 2 ** 14),
-#     'NG': ('result_Green.tif', lambda x: x / 2 ** 14),
-#     'NB': ('result_Blue.tif', lambda x: x / 2 ** 14),
-#     'RE': ('result_RedEdge.tif', lambda x: x / 2 ** 14),
-#     'NIR': ('result_NIR.tif', lambda x: x / 2 ** 14),
-# })
+BAND_MAX_CONFIG: dict[str, tuple[int, int]] = {
+    'WR': (0, 2 ** 8),
+    'WG': (0, 2 ** 8),
+    'WB': (0, 2 ** 8),
+    'NR': (0, 2 ** 14),
+    'NG': (0, 2 ** 14),
+    'NB': (0, 2 ** 14),
+    'RE': (0, 2 ** 14),
+    'NIR': (0, 2 ** 14),
+}
