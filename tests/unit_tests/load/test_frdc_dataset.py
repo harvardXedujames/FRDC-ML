0b238995ebdb939ee3571857e93857b5b6d56ce0
Evening <johnchangqi@hotmail.com>
2023-10-11 10:13:13 +0800

    Add test for ordering behavior

--- tests/unit_tests/load/test_frdc_dataset.py
+++ tests/unit_tests/load/test_frdc_dataset.py
@@ -7,8 +7,16 @@ def test_get_ar_bands_as_dict(ds):
     assert set(d.keys()) == set(d.keys())
 
 
-def test_get_ar_bands(ar, order):
-    assert ar.shape[-1] == len(order)
+def test_get_ar_bands(ds):
+    ar, order = ds.get_ar_bands()
+    assert ar.shape[-1] == len(BAND_CONFIG)
+    assert order == list(BAND_CONFIG.keys())
+
+
+def test_get_ar_bands_ordering(ds):
+    ar, order = ds.get_ar_bands(['WB', 'WG'])
+    assert ar.shape[-1] == 2
+    assert order == ['WB', 'WG']
 
 
 def test_get_bounds(ds):
