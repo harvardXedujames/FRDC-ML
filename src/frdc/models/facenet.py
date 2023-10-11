4760b83997fe09f0a254dfb0b8cf58c6d3eae6fe
Evening <johnchangqi@hotmail.com>
2023-10-11 15:48:48 +0800

    Fix incorrect channel size check

--- src/frdc/models/facenet.py
+++ src/frdc/models/facenet.py
@@ -5,27 +5,35 @@ from torchvision.models import Inception_V3_Weights, inception_v3
 
 class FaceNet(nn.Module):
     INCEPTION_OUT_DIMS = 1524
+    INCEPTION_IN_CHANNELS = 3
     MIN_SIZE = 299
-    CHANNELS = 3
 
-    def __init__(self, n_classes: int = 10):
+    def __init__(self,
+                 n_in_channels: int = 8,
+                 n_out_classes: int = 10):
         """ Initialize the FaceNet model.
+
+        Args:
+            n_in_channels: The number of input channels (bands)
+            n_out_classes: The number of output classes
 
         Notes:
             - Min input size: 299 x 299.
-            - Channels: 3.
             - Batch size: >= 2.
 
             Retrieve these constants in class attributes MIN_SIZE and CHANNELS.
         """
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3,
+        self.conv1 = nn.Conv2d(in_channels=n_in_channels,
+                               out_channels=10, kernel_size=3,
                                padding=1)
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(in_channels=10, out_channels=7, kernel_size=3,
                                padding=1)
         self.relu2 = nn.ReLU()
-        self.conv3 = nn.Conv2d(in_channels=7, out_channels=3, kernel_size=1)
+        self.conv3 = nn.Conv2d(in_channels=7,
+                               out_channels=self.INCEPTION_IN_CHANNELS,
+                               kernel_size=1)
         self.relu3 = nn.ReLU()
 
         self.base_model = inception_v3(
@@ -47,14 +55,13 @@ class FaceNet(nn.Module):
 
         # Logits & aux_logits are the shape of (batch_size, INCEPTION_OUT_DIMS)
         # Thus concat them to (batch_size, INCEPTION_OUT_DIMS * 2)
-        self.fc = nn.Linear(self.INCEPTION_OUT_DIMS * 2, n_classes)
+        self.fc = nn.Linear(self.INCEPTION_OUT_DIMS * 2, n_out_classes)
 
     def forward(self, x: torch.Tensor):
         """ Forward pass.
 
         Notes:
             - Min input size: 299 x 299.
-            - Channels: 3.
             - Batch size: >= 2.
 
         Args:
@@ -63,14 +70,12 @@ class FaceNet(nn.Module):
 
         if (
                 x.shape[0] < 2 or
-                x.shape[1] != self.CHANNELS or
                 x.shape[2] < self.MIN_SIZE or
                 x.shape[3] < self.MIN_SIZE
         ):
-            raise ValueError(
+            raise RuntimeError(
                 f'Input shape {x.shape} must adhere to the following:\n'
                 f' - Batch size >= 2\n'
-                f' - Channels == {self.CHANNELS}\n'
                 f' - Height >= {self.MIN_SIZE}\n'
             )
         logits, aux_logits = self.feature_extraction(x)
--- tests/unit_tests/models/test_facenet.py
+++ tests/unit_tests/models/test_facenet.py
@@ -4,33 +4,33 @@ import torch
 from frdc.models import FaceNet
 
 N_CLASSES = 42
+N_CHANNELS = 2
 BATCH_SIZE = 2
 MIN_SIZE = FaceNet.MIN_SIZE
-CHANNELS = FaceNet.CHANNELS
 
 
 @pytest.fixture(scope='module')
 def face_net():
-    return FaceNet(n_classes=N_CLASSES)
+    return FaceNet(n_in_channels=N_CHANNELS, n_out_classes=N_CLASSES)
 
 
 @pytest.mark.parametrize(
     ['batch_size', 'channels', 'size', 'ok'],
     [
-        [BATCH_SIZE, CHANNELS, MIN_SIZE, True],
-        [BATCH_SIZE, CHANNELS, MIN_SIZE + 1, True],
-        [BATCH_SIZE, CHANNELS - 1, MIN_SIZE, False],
-        [BATCH_SIZE, CHANNELS + 1, MIN_SIZE, False],
-        [BATCH_SIZE, CHANNELS, MIN_SIZE - 1, False],
-        [BATCH_SIZE, CHANNELS - 1, MIN_SIZE - 1, False],
-        [BATCH_SIZE, CHANNELS + 1, MIN_SIZE - 1, False],
-        [1, CHANNELS, MIN_SIZE, False],
-        [1, CHANNELS, MIN_SIZE + 1, False],
-        [1, CHANNELS - 1, MIN_SIZE, False],
-        [1, CHANNELS + 1, MIN_SIZE, False],
-        [1, CHANNELS, MIN_SIZE - 1, False],
-        [1, CHANNELS - 1, MIN_SIZE - 1, False],
-        [1, CHANNELS + 1, MIN_SIZE - 1, False],
+        [BATCH_SIZE, N_CHANNELS, MIN_SIZE, True],
+        [BATCH_SIZE, N_CHANNELS, MIN_SIZE + 1, True],
+        [BATCH_SIZE, N_CHANNELS - 1, MIN_SIZE, False],
+        [BATCH_SIZE, N_CHANNELS + 1, MIN_SIZE, False],
+        [BATCH_SIZE, N_CHANNELS, MIN_SIZE - 1, False],
+        [BATCH_SIZE, N_CHANNELS - 1, MIN_SIZE - 1, False],
+        [BATCH_SIZE, N_CHANNELS + 1, MIN_SIZE - 1, False],
+        [1, N_CHANNELS, MIN_SIZE, False],
+        [1, N_CHANNELS, MIN_SIZE + 1, False],
+        [1, N_CHANNELS - 1, MIN_SIZE, False],
+        [1, N_CHANNELS + 1, MIN_SIZE, False],
+        [1, N_CHANNELS, MIN_SIZE - 1, False],
+        [1, N_CHANNELS - 1, MIN_SIZE - 1, False],
+        [1, N_CHANNELS + 1, MIN_SIZE - 1, False],
     ]
 )
 def test_face_net_io(face_net, batch_size, channels, size, ok):
@@ -38,5 +38,5 @@ def test_face_net_io(face_net, batch_size, channels, size, ok):
     if ok:
         assert face_net(x).shape == (BATCH_SIZE, N_CLASSES)
     else:
-        with pytest.raises(ValueError):
+        with pytest.raises(RuntimeError):
             face_net(x)
