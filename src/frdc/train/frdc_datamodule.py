19cc204f87047985ed7206758e449b27bb669e3f
Evening <johnchangqi@hotmail.com>
2023-10-12 12:29:23 +0800

    Reduce scope of dm signature

--- src/frdc/train/frdc_datamodule.py
+++ src/frdc/train/frdc_datamodule.py
@@ -5,62 +5,77 @@ from typing import Callable
 import numpy as np
 import torch
 from lightning import LightningDataModule
-from skimage.transform import resize
 from sklearn.preprocessing import LabelEncoder
-from torch.utils.data import DataLoader, TensorDataset, random_split, Subset
-
-from frdc.load import FRDCDataset
-from frdc.preprocess import extract_segments_from_bounds
+from torch.utils.data import DataLoader, TensorDataset, Subset
 
 
 class FRDCDataModule(LightningDataModule):
     def __init__(
             self,
-            ds: FRDCDataset,
-            ar_segment_transform: Callable[
-                [list[np.ndarray]], torch.Tensor
-            ],
-            td_split: Callable[
-                [TensorDataset], list[Subset, Subset, Subset]
-            ],
+            *,
+            segments: list[np.ndarray],
+            fn_segment_tf: Callable[[list[np.ndarray]], torch.Tensor],
+            labels: list[str] | None = None,
+            fn_split: Callable[[TensorDataset], list[Subset, Subset, Subset]] =
+                lambda x: [x, x, x],
             batch_size: int = 4,
     ):
-        """
+        """ FRDC Data Module.
+
+        Notes:
+            We separate the segment input and transform as we expect the input
+            to be as "raw" as possible, which is usually in the np.ndarray
+            format.
+
+            This makes it easy if we have custom images with alternative
+            segmentations, such as through an auto-segmentation.
 
         Args:
-            ds: The FRDCDataset to use
-            ar_segment_transform: Transform applied to the segments.
-                It takes a list of segments and return a batched tensor:
+            segments: A list of segments, each ND-Array.
+                Note that PyTorch expects input shapes of (B, C, H, W),
+                 while common image libs expect shapes (H, W, C).
+            labels: A list of labels as strings. If None, then the datamodule
+                will not have train, val, test datasets.
+            fn_segment_tf: Transform applied to the segments.
+                It takes a list of segments, returning a batched tensor:
                  (batch, height, width, channels).
-                In the case of FRDC each segment is the crown segment of each
-                tree, the output should be a batched image of all the crown
-                segments.
-            td_split: This is a function that takes a TensorDataset and splits
+                In FRDC, each segment is the tree crown segment, the output
+                 should be a batched image of all the crown segments.
+                See Examples for more details.
+            fn_split: This is a function that takes a TensorDataset and splits
                 it into train, val, test TensorDatasets.
+                See Examples for more details.
             batch_size: The batch size to use for the dataloaders.
 
         Examples:
-            The ar_transform could be a function that resizes the segments to
-            299x299 and then stacks them into a batched image:
+            The fn_segment_tf could be a function that resizes the segments to
+            299x299 and then stacks them into a batched image. The output is
+            then permuted to be (batch, channels, height, width).:
 
-            # >>> def ar_transform(segments):
-            # >>>     segments = [resize(s, [299, 299]) for s in segments]
-            # >>>     return np.stack(segments)
+            >>> from skimage.transform import resize
+            >>> from frdc.models import FaceNet
+            >>>
+            >>> fn_segment_tf=lambda x: torch.stack([
+            >>>     torch.from_numpy(
+            >>>         resize(s, [FaceNet.MIN_SIZE, FaceNet.MIN_SIZE])
+            >>>     ) for s in x
+            >>> ]).permute(0, 3, 1, 2)
+
+            The fn_split could be a function that splits the dataset into
+            train, val, test subsets.:
+
+            >>> from torch.utils.data import random_split
+            >>>
+            >>> fn_split=lambda x: random_split(x, lengths=[len(x) - 6, 3, 3])
+
         """
         super().__init__()
-        self.ds = ds
-        self.ar_segment_transform = ar_segment_transform
-        self.td_split = td_split
+        self.segments = segments
+        self.labels = labels
         self.le = LabelEncoder()
         self.batch_size = batch_size
-
-    def prepare_data(self):
-        ar, order = self.ds.get_ar_bands()
-        bounds, labels = self.ds.get_bounds_and_labels()
-        ar_segments = extract_segments_from_bounds(ar, bounds, cropped=True)
-        # self.ar has the shape (batch, height, width, channels)
-        self.t = self.ar_segment_transform(ar_segments)
-        self.y = torch.from_numpy(self.le.fit_transform(labels))
+        self.fn_segment_tf = fn_segment_tf
+        self.fn_split = fn_split
 
     def setup(self, stage=None):
         # Split dataset into train, val, test
@@ -79,4 +94,6 @@ class FRDCDataModule(LightningDataModule):
         return DataLoader(self.test_ds, batch_size=self.batch_size,
                           shuffle=False)
 
-
+    def predict_dataloader(self):
+        return DataLoader(self.predict_ds, batch_size=self.batch_size,
+                          shuffle=False)
