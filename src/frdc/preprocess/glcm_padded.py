466c19a3080861cdd44252afa6ca1b03d980de35
Evening <johnchangqi@hotmail.com>
2023-10-20 09:42:01 +0800

    Add helper function to append glcm directly

--- src/frdc/preprocess/glcm_padded.py
+++ src/frdc/preprocess/glcm_padded.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Hashable
+from typing import Hashable, Collection
 
 import numpy as np
 from glcm_cupy import glcm, Features
@@ -20,7 +20,7 @@ def glcm_padded(
         bin_to: int,
         radius: int,
         step_size: int = 1,
-        features: Features | None = None,
+        features: Collection[Features] | None = None,
         **kwargs
 ) -> np.ndarray:
     """ A wrapper for glcm-cupy's glcm. This pads the GLCM automatically
@@ -79,7 +79,7 @@ def glcm_padded_cached(
         bin_to: int,
         radius: int,
         step_size: int = 1,
-        features: Features | None = None,
+        features: Collection[Features] | None = None,
         **kwargs):
     """ A wrapper for glcm-cupy's glcm. This pads the GLCM automatically
 
@@ -103,3 +103,45 @@ def glcm_padded_cached(
     return glcm_padded(ar, bin_from=bin_from, bin_to=bin_to, radius=radius,
                        step_size=step_size, features=features,
                        **kwargs)
+
+
+def append_glcm_padded_cached(
+        ar: np.ndarray,
+        *,
+        bin_from: int,
+        bin_to: int,
+        radius: int,
+        step_size: int = 1,
+        features: Collection[Features] | None = None,
+        **kwargs) -> np.ndarray:
+    """ This is a wrapper around glcm_padded_cached that appends the GLCM
+    features to the channels.
+
+    Notes:
+        This function is also cached. The cache is invalidated if any of the
+        arguments change.
+
+        I wanted to avoid this function as it may seem redundant, but it
+        significantly improves the readability of the code.
+
+    Returns:
+        Given an input (H, W, C), returns a (H, W, C * GLCM Features) array
+    """
+    ar_glcm = glcm_padded_cached(
+        ar,
+        step_size=step_size, bin_from=bin_from, bin_to=bin_to,
+        radius=radius, features=features, **kwargs
+    )
+
+    # : (H, W, C), s_glcm: (H, W, C, GLCM Features)
+    # Concatenate the GLCM features to the channels
+    return np.concatenate(
+        [
+            # (H, W, C)
+            ar,
+            # ar_glcm is of shape (H, W, C, GLCM Features)
+            # We reshape it to (H, W, C * GLCM Features)
+            ar_glcm.reshape(*ar_glcm.shape[:-2], -1)
+        ],
+        axis=-1
+    )
