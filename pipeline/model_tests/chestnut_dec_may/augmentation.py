8cbefd929c0c49d19bf09418a12a00f162a28568
Evening <johnchangqi@hotmail.com>
2023-10-20 19:01:46 +0800

    Update augmentation.py

--- pipeline/model_tests/chestnut_dec_may/augmentation.py
+++ pipeline/model_tests/chestnut_dec_may/augmentation.py
@@ -3,6 +3,7 @@ from torchvision.transforms.v2 import RandomHorizontalFlip, RandomVerticalFlip
 
 
 def augmentation(t: torch.Tensor) -> torch.Tensor:
+    """Runs out augmentation on a tensor."""
     t = RandomHorizontalFlip()(t)
     t = RandomVerticalFlip()(t)
     return t
