80e3e6a6c4eb28a1c92fb46fe87b4a9d14b3b22d
Evening <johnchangqi@hotmail.com>
2023-11-29 15:19:07 +0800

    Update script to run with new dm

--- tests/model_tests/chestnut_dec_may/main.py
+++ tests/model_tests/chestnut_dec_may/main.py
@@ -3,80 +3,86 @@
 This test is done by training a model on the 20201218 dataset, then testing on
 the 20210510 dataset.
 """
+import os
 from pathlib import Path
 
 import lightning as pl
 import numpy as np
 import torch
+import wandb
 from lightning.pytorch.callbacks import (
     LearningRateMonitor,
     ModelCheckpoint,
     EarlyStopping,
 )
-from torch.utils.data import TensorDataset, Dataset, Subset
-
-from frdc.models import InceptionV3
-from frdc.train import FRDCDataModule, FRDCModule
-from tests.model_tests.chestnut_dec_may.augmentation import augmentation
-from tests.model_tests.chestnut_dec_may.preprocess import preprocess
-from tests.model_tests.utils import get_dataset
 from lightning.pytorch.loggers import WandbLogger
-import wandb
-
-assert wandb.run is None
-
-wandb.setup(wandb.Settings(program=__name__, program_relpath=__name__))
-run = wandb.init()
-logger = WandbLogger(name="chestnut_dec_may", project="frdc")
-
-
-def train_val_test_split(
-    x: TensorDataset,
-) -> list[Dataset, Dataset, Dataset]:
-    # Defines how to split the dataset into train, val, test subsets.
-    # TODO: Quite ugly as it uses the global variables segments_0 and
-    #  segments_1. Will need to refactor this.
-    return [
-        Subset(x, list(range(len(segments_0)))),
-        Subset(
-            x,
-            list(range(len(segments_0), len(segments_0) + len(segments_1))),
-        ),
-        [],
-    ]
-
-
-# Prepare the dataset
-segments_0, labels_0 = get_dataset("chestnut_nature_park", "20201218", None)
-segments_1, labels_1 = get_dataset(
-    "chestnut_nature_park", "20210510", "90deg43m85pct255deg/map"
+from torchvision.transforms import RandomVerticalFlip
+from torchvision.transforms.v2 import (
+    Compose,
+    ToImage,
+    ToDtype,
+    RandomHorizontalFlip,
+    Normalize,
+    Resize,
 )
 
+from frdc.load import FRDCDataset
+from frdc.load.dataset import FRDCConcatDataset
+from frdc.models import InceptionV3
+from frdc.train import FRDCModule
+from frdc.train.frdc_datamodule_new import FRDCDataModule, Transforms
 
-# Concatenate the datasets
-segments = [*segments_0, *segments_1]
-labels = [*labels_0, *labels_1]
+os.environ["GOOGLE_CLOUD_PROJECT"] = "frmodel"
+# assert wandb.run is None
+#
+# wandb.setup(wandb.Settings(program=__name__, program_relpath=__name__))
+# run = wandb.init()
+# logger = WandbLogger(name="chestnut_dec_may", project="frdc")
+
+# Prepare the dataset
+ds0 = FRDCDataset(
+    "chestnut_nature_park",
+    "20201218",
+    None,
+)
+ds1 = FRDCDataset(
+    "chestnut_nature_park", "20210510", "90deg43m85pct255deg/map"
+)
+mean = np.nanmean(ds0.ar, axis=(0, 1))
+std = np.nanstd(ds0.ar, axis=(0, 1))
+
+ds = FRDCConcatDataset([ds0, ds1])
 
 BATCH_SIZE = 5
 EPOCHS = 50
 LR = 1e-3
 
+
+def tf(x):
+    return Compose(
+        [
+            ToImage(),
+            ToDtype(torch.float32, scale=True),
+            Resize(
+                [InceptionV3.MIN_SIZE, InceptionV3.MIN_SIZE], antialias=True
+            ),
+            RandomHorizontalFlip(),
+            RandomVerticalFlip(),
+            Normalize(mean=mean, std=std),
+        ]
+    )(x)
+
+
 # Prepare the datamodule and trainer
 dm = FRDCDataModule(
-    # Input to the model
-    segments=segments,
-    # Output of the model
-    labels=labels,
-    # Preprocessing function
-    preprocess=preprocess,
-    # Augmentation function (Only on train)
-    augmentation=augmentation,
-    # Splitting function
-    train_val_test_split=train_val_test_split,
-    # Batch size
+    ds=ds,
+    transforms=Transforms(
+        train_tf=tf,
+        val_tf=tf,
+    ),
     batch_size=BATCH_SIZE,
 )
-
+# %%
 trainer = pl.Trainer(
     max_epochs=EPOCHS,
     # fast_dev_run=True,
@@ -84,7 +90,7 @@ trainer = pl.Trainer(
     # TODO: Though this is set, the results are still not reproducible.
     deterministic=True,
     # fast_dev_run=True,
-    accelerator="gpu",
+    # accelerator="gpu",
     log_every_n_steps=4,
     callbacks=[
         # Stop training if the validation loss doesn't improve for 4 epochs
@@ -94,15 +100,11 @@ trainer = pl.Trainer(
         # Save the best model
         ModelCheckpoint(monitor="val_loss", mode="min", save_top_k=1),
     ],
-    logger=logger,
+    # logger=logger,
 )
 
 m = FRDCModule(
-    # Our model is the "FaceNet" model
-    # TODO: It's not really the FaceNet model,
-    #  but a modified version of it.
-    model_f=lambda: InceptionV3(n_out_classes=len(set(labels))),
-    # We use the Adam optimizer
+    model_f=lambda: InceptionV3(n_out_classes=len(set(ds.targets))),
     optim_f=lambda model: torch.optim.Adam(model.parameters(), lr=LR),
     scheduler_f=lambda optim: torch.optim.lr_scheduler.ExponentialLR(
         optimizer=optim,
@@ -114,14 +116,13 @@ trainer.fit(m, datamodule=dm)
 # TODO: Quite hacky, but we need to save the label encoder for prediction.
 np.save("le.npy", dm.le.classes_)
 
-report = f"""
-# Chestnut Nature Park (Dec 2020 vs May 2021)
-[WandB Report]({run.get_url()})
-TODO: Authentication for researchers
-"""
-
-with open(Path(__file__).parent / "report.md", "w") as f:
-    f.write(report)
-
-
-wandb.finish()
+# report = f"""
+# # Chestnut Nature Park (Dec 2020 vs May 2021)
+# [WandB Report]({run.get_url()})
+# TODO: Authentication for researchers
+# """
+#
+# with open(Path(__file__).parent / "report.md", "w") as f:
+#     f.write(report)
+#
+# wandb.finish()
