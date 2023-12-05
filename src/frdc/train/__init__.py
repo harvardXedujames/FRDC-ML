24bde345ebfb95d856a9398e3c3b7f620b9ae96c
Evening <johnchangqi@hotmail.com>
2023-12-05 18:12:08 +0800

    Remove deprecated modules

--- src/frdc/train/__init__.py
+++ src/frdc/train/__init__.py
@@ -1,5 +1,3 @@
 from .frdc_datamodule import FRDCDataModule
-from .frdc_module import FRDCModule
-from .train import dummy_train
 
-__all__ = ["dummy_train", "FRDCDataModule", "FRDCModule"]
+__all__ = ["FRDCDataModule"]
