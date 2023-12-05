ec1c39ebd61f8d8f3d453959962ce29445687e49
Evening <johnchangqi@hotmail.com>
2023-12-05 17:29:14 +0800

    Add a new datamodule for SSL

--- src/frdc/train/frdc_datamodule.py
+++ src/frdc/train/frdc_datamodule.py
@@ -1,23 +1,14 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
-import torch
 from lightning import LightningDataModule
-from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import DataLoader, RandomSampler
-from torchvision.transforms.v2 import (
-    Compose,
-    ToImage,
-    ToDtype,
-)
 
 from frdc.load import FRDCDataset
 
-ToTensor = Compose([ToImage(), ToDtype(torch.float32, scale=True)])
 
-
-@dataclass  # (kw_only=True) # only available when we use Py3.10
+@dataclass
 class FRDCDataModule(LightningDataModule):
     """FRDC Data Module.
 
@@ -32,16 +23,8 @@ class FRDCDataModule(LightningDataModule):
     train_iters: int = 100
     val_iters: int = 100
 
-    le: LabelEncoder = field(init=False, default=LabelEncoder())
-
     def __post_init__(self):
         super().__init__()
-
-    def setup(self, stage: str) -> None:
-        # TODO: We'll figure out the test set later.
-        #       Our dataset is way too small, even if we create one, it'll
-        #       be too small to be useful.
-        ...
 
     def train_dataloader(self):
         return DataLoader(
@@ -53,6 +36,54 @@ class FRDCDataModule(LightningDataModule):
                 replacement=False,
             ),
         )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_ds,
+            batch_size=self.batch_size,
+        )
+
+
+@dataclass
+class FRDCSSLDataModule(LightningDataModule):
+    """FRDC Data Module.
+
+    Args:
+        batch_size: The batch size to use for the dataloaders.
+
+    """
+
+    train_lab_ds: FRDCDataset
+    train_unl_ds: FRDCDataset
+    val_ds: FRDCDataset
+    batch_size: int = 4
+    train_iters: int = 100
+    val_iters: int = 100
+
+    def __post_init__(self):
+        super().__init__()
+
+    def train_dataloader(self):
+        return [
+            DataLoader(
+                self.train_lab_ds,
+                batch_size=self.batch_size,
+                sampler=RandomSampler(
+                    self.train_lab_ds,
+                    num_samples=self.batch_size * self.train_iters,
+                    replacement=False,
+                ),
+            ),
+            DataLoader(
+                self.train_unl_ds,
+                batch_size=self.batch_size,
+                sampler=RandomSampler(
+                    self.train_unl_ds,
+                    num_samples=self.batch_size * self.train_iters,
+                    replacement=False,
+                ),
+            ),
+        ]
 
     def val_dataloader(self):
         return DataLoader(
