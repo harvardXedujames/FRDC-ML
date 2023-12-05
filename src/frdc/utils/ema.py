97925372242d4a80a42e7929526c8e9ef36cd971
Evening <johnchangqi@hotmail.com>
2023-12-05 17:30:25 +0800

    Add EMA

---
+++ src/frdc/utils/ema.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+
+class WeightEMA:
+    def __init__(
+        self,
+        model: nn.Module,
+        ema_model: nn.Module,
+    ):
+        self.model = model
+        self.ema_model = ema_model
+
+    def update(self, lr: float):
+        """Update the EMA model with the current model's parameters.
+
+        Args:
+            lr: A fraction controlling how much should the EMA learn from the
+                current model.
+        """
+        for param, ema_param in zip(
+            self.model.parameters(), self.ema_model.parameters()
+        ):
+            if ema_param.dtype == torch.float32:
+                ema_param.mul_(1 - lr)
+                ema_param.add_(param * lr)
