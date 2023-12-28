76b4dff776745a84badcb3fd41ebfb33ca472fc3
Evening <johnchangqi@hotmail.com>
2023-12-28 14:48:19 +0800

    Update debug dataset loading

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,12 @@ import numpy as np
 import pytest
 
 from frdc.load import FRDCDataset
+from frdc.load.dataset import FRDCDatasetPreset
 
 
 @pytest.fixture(scope="session")
 def ds() -> FRDCDataset:
-    return FRDCDataset._load_debug_dataset()
+    return FRDCDatasetPreset.DEBUG()
 
 
 @pytest.fixture(scope="session")
