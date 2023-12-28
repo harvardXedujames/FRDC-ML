7d3183e86aeb790fb00c6617d7a01bfb2304b033
Evening <johnchangqi@hotmail.com>
2023-12-28 17:24:49 +0800

    Implement auto casting of labelled to unlabelled
    This provides a failsafe interface if somehow someone forgot to use the unlabelled set, which is totally fine

--- src/frdc/train/frdc_datamodule.py
+++ src/frdc/train/frdc_datamodule.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from types import MethodType
+from typing import Any
 
 from lightning import LightningDataModule
 from torch.utils.data import DataLoader, RandomSampler
 
 from frdc.load import FRDCDataset
+from frdc.load.dataset import FRDCUnlabelledDataset
 
 
 @dataclass
@@ -14,34 +17,39 @@ class FRDCDataModule(LightningDataModule):
 
     Notes:
         This is a special datamodule for semi-supervised learning, which
-        requires two dataloaders for the labelled and unlabelled datasets.
-        It can also be used for supervised learning, by passing in None for
-        the unlabelled dataset.
+        can accept an optional dataloaders for an unlabelled dataset.
+
+        Without an unsupervised dataset it can be used for supervised learning,
+        by passing in None for the unlabelled dataset.
 
         If you're using our MixMatch Module, using None for the unlabelled
         dataset will skip the MixMatch. However, note that this is not
         equivalent to passing the Labelled set as unlabelled as well.
 
-        For example:
-        >>> FRDCSSLDataModule(
-        ...     train_lab_ds=train_lab_ds,
-        ...     train_unl_ds=train_lab_ds,
-        ...     ...
-        ... )
+        For example::
 
-        Does not have the same performance as:
-        >>> FRDCSSLDataModule(
-        ...     train_lab_ds=train_lab_ds,
-        ...     train_unl_ds=None,
-        ...     ...
-        ... )
+            FRDCDataModule(
+                train_lab_ds=train_lab_ds,
+                train_unl_ds=train_lab_ds,
+                ...
+            )
+
+        Does not have the same performance as::
+
+            FRDCSSLDataModule(
+                train_lab_ds=train_lab_ds,
+                train_unl_ds=None,
+                ...
+            )
 
         As partially, some samples in MixMatch uses the unlabelled loss.
 
     Args:
         train_lab_ds: The labelled training dataset.
         train_unl_ds: The unlabelled training dataset. Can be None, which will
-            default to a DataModule suitable for supervised learning.
+            default to a DataModule suitable for supervised learning. If
+            train_unl_ds is a FRDCDataset, it will be converted to a
+            FRDCUnlabelledDataset, which simply masks away the labels.
         val_ds: The validation dataset.
         batch_size: The batch size to use for the dataloaders.
         train_iters: The number of iterations to run for the labelled training
@@ -52,13 +60,16 @@ class FRDCDataModule(LightningDataModule):
 
     train_lab_ds: FRDCDataset
     val_ds: FRDCDataset
-    train_unl_ds: FRDCDataset | None = None
+    train_unl_ds: FRDCDataset | FRDCUnlabelledDataset | None = None
     batch_size: int = 4
     train_iters: int = 100
     val_iters: int = 100
 
     def __post_init__(self):
         super().__init__()
+
+        if isinstance(self.train_unl_ds, FRDCDataset):
+            self.train_unl_ds.__class__ = FRDCUnlabelledDataset
 
     def train_dataloader(self):
         num_samples = self.batch_size * self.train_iters
